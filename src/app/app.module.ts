007f145df02b314b123874a947d3bb682dc1819d
Martin Gontšarov <qwertyuk01@gmail.com>
2020-02-11 19:52:12 +0200

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiscordComponent } from './discord/discord.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiscordComponent,
+    HeaderComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
