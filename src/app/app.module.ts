521c865cdd532cee215a347e1ff2613eb8790377
BuildTools <unconfigured@null.spigotmc.org>
2020-02-13 18:33:31 +0200

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RainComponent } from './rain/rain.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { RainComponent } from './rain/rain.component';
     HeaderComponent,
     MainComponent,
     SidebarComponent,
-    RainComponent
+    RainComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
