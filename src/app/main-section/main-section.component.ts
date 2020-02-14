b221c809e60b2d9dabe01f1a348a8d3687ec5676
Martin Gontšarov <qwertyuk01@gmail.com>
2020-02-14 23:30:13 +0200

    Styling

--- src/app/main-section/main-section.component.ts
+++ src/app/main-section/main-section.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery'
 @Component({
   selector: 'app-main-section',
   templateUrl: './main-section.component.html',
@@ -10,6 +10,18 @@ export class MainSectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.trulz-text').hover(e=>{
+      console.log(e)
+      if (e.type == 'mouseenter') {
+        $(e.target).addClass('trulz-zoom')
+        $(e.target).removeClass('trulz-glow')
+
+      } else {
+        $(e.target).removeClass('trulz-zoom')
+        $(e.target).addClass('trulz-glow')
+
+      }
+    })
   }
 
 }
